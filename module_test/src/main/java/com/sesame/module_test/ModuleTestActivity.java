4509507afeeaeb9e8d78e89cd88e824b290a4650
KanBin <kanbin@jujin8.com>
2021-07-29 10:11:33 +0800

    组件化——配置全局gradle环境

---
+++ module_test/src/main/java/com/sesame/module_test/ModuleTestActivity.java
@@ -0,0 +1,14 @@
+package com.sesame.module_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModuleTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module_test);
+    }
+}
