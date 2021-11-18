9e55203cff2d65ad0d452017b619f1ab39d383e0
KanBin <kanbin@jujin8.com>
2021-11-18 18:23:57 +0800

    创建aidl_client布局

--- aidl_client/src/main/java/com/sesame/aidl_client/MainActivity.java
+++ aidl_client/src/main/java/com/sesame/aidl_client/MainActivity.java
@@ -1,15 +1,49 @@
 package com.sesame.aidl_client;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private EditText mEtNum1;
+    private EditText mEtNum2;
+    private Button mBtnCommit;
+    private TextView mTvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEtNum1 = findViewById(R.id.etNum1);
+        mEtNum2 = findViewById(R.id.etNum2);
+        mBtnCommit = findViewById(R.id.btnCommit);
+        mTvResult = findViewById(R.id.tvResult);
+
+        mBtnCommit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnCommit:
+                String s1 = mEtNum1.getText().toString();
+                String s2 = mEtNum2.getText().toString();
+                if (TextUtils.isEmpty(s1) || TextUtils.isEmpty(s2) || !TextUtils.isDigitsOnly(s1) || !TextUtils.isDigitsOnly(s1)){
+                    Toast.makeText(this, "输入有误", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int a = Integer.parseInt(s1);
+                int b = Integer.parseInt(s2);
+                mTvResult.setText(String.valueOf(a * b));
+                break;
+        }
     }
 }
--- aidl_client/src/main/java/com/sesame/aidl_client/aidl.md
+++ aidl_client/src/main/java/com/sesame/aidl_client/aidl.md
@@ -1,3 +1,7 @@
+# 说明
+本例子要展示的是进程间通信，客户端输入两个数字a和b，服务端返回a*b的计算结果。
+
 # AndroidIPC 机制
 https://github.com/leavesC/IPCSamples/blob/master/note/AndroidIPC%E6%9C%BA%E5%88%B6%EF%BC%883%EF%BC%89-AIDL.md
 
+# aidl流程
