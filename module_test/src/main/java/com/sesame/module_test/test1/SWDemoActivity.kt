d13fcaca6a5a5a0f968a4d70827640ee839e057e
KanBin <kanbin@jujin8.com>
2021-11-23 17:11:43 +0800

    屏幕适配-最小宽度限定符

---
+++ module_test/src/main/java/com/sesame/module_test/test1/SWDemoActivity.kt
@@ -0,0 +1,14 @@
+package com.sesame.module_test.test1
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sesame.module_test.R
+
+class SWDemoActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo_sw)
+    }
+}
