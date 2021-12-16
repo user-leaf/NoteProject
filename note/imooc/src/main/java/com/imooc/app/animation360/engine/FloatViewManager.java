5b9d3db8db41689c01d278c6401c680c9fdd1e77
KanBin <kanbin@jujin8.com>
2021-12-16 15:37:15 +0800

    浮窗小球的事件处理之拖拽

--- note/imooc/src/main/java/com/imooc/app/animation360/engine/FloatViewManager.java
+++ note/imooc/src/main/java/com/imooc/app/animation360/engine/FloatViewManager.java
@@ -3,6 +3,8 @@ package com.imooc.app.animation360.engine;
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.imooc.app.animation360.view.FloatCircleView;
@@ -12,11 +14,58 @@ public class FloatViewManager {
     private Context mContext;
     private FloatCircleView mCircleView;
     private final WindowManager mWindowManager; // 通过这个windowmanager来操控浮窗体的显示和隐藏以及位置的改变
+    private View.OnTouchListener mCircleViewTouchListener = new View.OnTouchListener() {
+
+        private float mStartX;
+        private float mStartY;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            // touch三个动作的监听
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    mStartX = event.getRawX();
+                    mStartY = event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    float x = event.getRawX();
+                    float y = event.getRawY();
+                    float dx = x - mStartX;
+                    float dy = y - mStartY;
+                    mParams.x += dx;
+                    mParams.y += dy;
+                    mWindowManager.updateViewLayout(mCircleView, mParams);
+                    mStartX = x;
+                    mStartY = y;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    // 手指离开的时候
+                    float x1 = event.getRawX();
+                    if (x1 > getScreenWidth() / 2) {
+                        mParams.x = getScreenWidth() - mCircleView.width; // todo params
+                    } else {
+                        mParams.x = 0;
+                    }
+                    mWindowManager.updateViewLayout(mCircleView, mParams);
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    };
+
+    public int getScreenWidth() {
+        return mWindowManager.getDefaultDisplay().getWidth(); // todo
+    }
+
+    private WindowManager.LayoutParams mParams;
 
     private FloatViewManager(Context context) {
         mContext = context;
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         mCircleView = new FloatCircleView(context);
+        mCircleView.setOnTouchListener(mCircleViewTouchListener);
     }
 
     private static FloatViewManager instance;
@@ -36,16 +85,16 @@ public class FloatViewManager {
      * 展示浮窗小球到窗口上
      */
     public void showFloatCircleView() {
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-        params.width = mCircleView.width;
-        params.height = mCircleView.height;
-        params.gravity = Gravity.TOP | Gravity.LEFT;
-        params.x = 0;
-        params.y = 0;
-        params.type = WindowManager.LayoutParams.TYPE_PHONE; // 像手机打电话界面始终处在最上方
-        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+        mParams = new WindowManager.LayoutParams();
+        mParams.width = mCircleView.width;
+        mParams.height = mCircleView.height;
+        mParams.gravity = Gravity.TOP | Gravity.LEFT;
+        mParams.x = 0;
+        mParams.y = 0;
+        mParams.type = WindowManager.LayoutParams.TYPE_PHONE; // 像手机打电话界面始终处在最上方
+        mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;// 特征 不去抢焦点
-        params.format = PixelFormat.RGBA_8888; // 背景透明
-        mWindowManager.addView(mCircleView, params);
+        mParams.format = PixelFormat.RGBA_8888; // 背景透明
+        mWindowManager.addView(mCircleView, mParams);
     }
 }
