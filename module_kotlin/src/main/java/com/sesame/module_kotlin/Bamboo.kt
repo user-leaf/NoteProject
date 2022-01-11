c42f888cfa1529d568e50c1432da490daa021deb
kanbin <kanbin@jujin8.com>
2022-01-11 22:26:27 +0800

    新建module_learn

---
+++ module_kotlin/src/main/java/com/sesame/module_kotlin/Bamboo.kt
@@ -0,0 +1,17 @@
+package com.sesame.module_kotlin
+
+class Bamboo {
+    companion object {
+        val name = "bamboo"
+        fun getAge(): Int {
+            return 18
+        }
+    }
+
+    val name2 = "bamboo2"
+
+}
+
+fun main() {
+    Bamboo.name
+}
