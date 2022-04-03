00b37b5463e04407082c08dfed21f8f1f94a72ef
kanbin <kanbin@live.com>
2022-04-03 21:16:11 +0800

    搜索-布局

---
+++ note/nano/src/main/java/com/bamboo/nano/search/SearchActivity.kt
@@ -0,0 +1,14 @@
+package com.bamboo.nano.search
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bamboo.nano.R
+
+class SearchActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+    }
+}
