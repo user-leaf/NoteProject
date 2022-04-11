b56d8b54be7bc54a037e6e5d00d534e05e9be446
KanBin <kanbin@jujin8.com>
2022-04-11 15:09:55 +0800

    .

--- module_test/src/main/java/com/sesame/module_test/test_flow2/FlowActivity.kt
+++ module_test/src/main/java/com/sesame/module_test/test_flow2/FlowActivity.kt
@@ -1,10 +1,13 @@
 package com.sesame.module_test.test_flow2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.sesame.module_test.ModuleTestActivity
 import com.sesame.module_test.R
+import kotlinx.android.synthetic.main.activity_flow2.*
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlin.system.measureTimeMillis
@@ -16,6 +19,12 @@ class FlowActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flow2)
+
+        btnJump.setOnClickListener {
+            startActivity(Intent(this, ModuleTestActivity::class.java))
+            overridePendingTransition(0,0)
+            this.finish()
+        }
 
         lifecycleScope.launch {
             viewModel.uiState.collect {
