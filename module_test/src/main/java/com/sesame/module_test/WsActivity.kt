44647f051eedd5a229c20af35c9e73a3e3235396
KanBin <kanbin@jujin8.com>
2022-04-11 18:43:39 +0800

    ws

---
+++ module_test/src/main/java/com/sesame/module_test/WsActivity.kt
@@ -0,0 +1,13 @@
+package com.sesame.module_test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_ws)
+    }
+
+}
